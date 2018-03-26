b19543b641372f9f3c3923744db97459840dd39d
alexander <sashayanin@gmail.com>
2018-03-26 15:41:03 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <gst/gst.h>
+#include <gst/sdp/sdp.h>
+
+#define GST_USE_UNSTABLE_API
+#include <gst/webrtc/webrtc.h>
+
+#include <libsoup/soup.h>
+#include <json-glib/json-glib.h>
+
+#include <string.h>
+
+int main (int argc, char *argv[]) {
+        printf("Testing gst webrtcbin plugin\n");
+}
